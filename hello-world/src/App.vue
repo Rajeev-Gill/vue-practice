dfae3cef163d2991c010e988c8c2ef24f0f4903c
Rajeev-Gill <rajeev.sgill@gmail.com>
2021-01-15 20:39:29 +0000

    added todos and todoitem component

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,34 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="This is a prop of the component HelloWorld.vue"/>
+    <!-- <HelloWorld msg="This is a prop of the component HelloWorld.vue"/>
     <Stonx msg="This is Jeevos Stonx component" />
-    <Tard msg="NGGGHHHHHGEEEENEHHFHHGGHH"/>
+    <Tard msg="NGGGHHHHHGEEEENEHHFHHGGHH"/> -->
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Stonx from './components/Stonks'
-import Tard from './components/Tard'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Stonx from './components/Stonks'
+// import Tard from './components/Tard'
+import Todos from './components/Todos'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Stonx,
-    Tard
+    //HelloWorld
+    // Stonx,
+    // Tard
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
 </style>
